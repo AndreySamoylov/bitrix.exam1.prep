b50bf280a7156ceee263ef80842c82b27782b950
samoylovanandreial <samoylovanandreial@gmail.com>
2024-02-29 08:43:47 +0500

    ex1-8

---
+++ .bottom_exam1.menu.php
@@ -0,0 +1,26 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$aMenuLinks = Array(
+	Array(
+		"Отзывы",
+		"rew/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Руководство",
+		"company/management/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+    Array(
+        "История",
+        "company/history/",
+        Array(),
+        Array(),
+        ""
+    ),
+);
+?>
